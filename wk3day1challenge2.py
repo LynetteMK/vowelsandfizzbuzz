17c42c900a4e610ce341d2af436f1436addb2675
mklynn20@gmail.com <mklynn20@gmail.com>
2018-11-12 20:17:03 +0300

    Add an introduction line

--- wk3day1challenge2.py
+++ wk3day1challenge2.py
@@ -1,4 +1,5 @@
-#Hello. A dictionary containing numbers ranging from 1to 15 and their squares.
+#A dictionary containing numbers ranging from 1to 15 and their squares.
+print("Hello. this is a dictionary containing numbers from 1 to 15 and their squares")
 squares = dict()
 
 for value in range(1,16):
