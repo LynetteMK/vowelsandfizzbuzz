d0ed05405a043a7344c2107319e1a339a7fb49b7
mklynn20@gmail.com <mklynn20@gmail.com>
2018-11-12 19:34:31 +0300

    dictionary commit

---
+++ wk3day1challenge2.py
@@ -0,0 +1,9 @@
+#Hello. A dictionary containing numbers ranging from 1to 15 and their squares.
+squares = dict()
+
+for value in range(1,16):
+    squares[value]=value**2
+
+print(squares)
+
+input()
